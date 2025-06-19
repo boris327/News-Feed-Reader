c4024f7d0ee2c3dee0ad0fe13b9708492cd3e6a3
boris327 <54038990+boris327@users.noreply.github.com>
2025-06-19 22:34:34 +0300

    Add files via upload

---
+++ custom_components/news_feed_reader/const.py
@@ -0,0 +1,10 @@
+DOMAIN = "news_feed_reader"
+
+DEFAULT_NAME = "חדשות ישראל"
+FEED_OPTIONS = {
+    "ynet_news": "https://www.ynet.co.il/Integration/StoryRss1854.xml",
+    "ynet_sport": "https://www.ynet.co.il/Integration/StoryRss3.xml",
+    "maariv": "https://www.maariv.co.il/Rss/RssFeedsMivzakiChadashot",
+    "walla": "https://rss.walla.co.il/feed/22",
+    "the_marker": "https://www.themarker.com/srv/tm-markets"
+}
